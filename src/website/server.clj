feda596f8b88015eebc7de34ffb77a6b1153c78b
Benedikt Kristinsson <benedikt.k@gmail.com>
2013-03-17 20:19:26 +0000

    commit for github

--- src/website/server.clj
+++ src/website/server.clj
@@ -4,7 +4,7 @@
 (server/load-views-ns 'website.views)
 
 (defn -main [& m]
-  (let [mode (keyword (or (first m) :prod))
+  (let [mode :prod
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
                         :ns 'website})))
--- src/website/views/photography.clj
+++ src/website/views/photography.clj
@@ -2,5 +2,19 @@
   (:require [website.views.common :as common]
             [noir.content.getting-started])
   (:use [noir.core :only [defpage defpartial]]
-        [hiccup.element :only [link-to]]
-        [org.satta.glob]))
+        [hiccup.element :only [link-to image]]
+        [org.satta.glob]
+        [clojure.string :only [split]]))
+
+(defpartial photo-list [img]
+  [:p (image (str "/thumbs/" img) "")])
+
+(defpage "/photography" []
+  (common/layout
+   [:p "This are a few photographs that I have taken."]
+   [:div#photos
+    (let [photos (map (fn [f]
+                        (last (split (.getCanonicalPath f) #"/")))
+                      (glob "resources/public/photos/*.jpg"))]
+      (map photo-list photos))]))
+
